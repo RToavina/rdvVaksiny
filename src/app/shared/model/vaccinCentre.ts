bda9cbc78a95b640e1a6ac8483629d5b9cf922aa
zohary13 <zoharyirimboangy@gmail.com>
2022-08-03 22:01:24 +0200

    vaccinodrome

---
+++ src/app/shared/model/vaccinCentre.ts
@@ -0,0 +1,18 @@
+export interface VaccinCentre {
+  idVaccinCentre: number;
+  idVaccinodrome: number;
+  idVaccin: number;
+  nomVaccin: string;
+  urlPhoto: string;
+  quantite: number;
+  nombreDose: number;
+  ageMinimum: number;
+  descriptions: string;
+  status: number;
+  nomCentre: string;
+  localisation: string;
+  email: string;
+  telephone: string;
+  adresse: string;
+  imgvaccinodrome: string;
+}
