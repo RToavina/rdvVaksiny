5a6b6aec51880499e293f5d4d97d4463ea7ee34d
Razanajatovo A. Toavina <razanajatovo.a.toavina@gmail.com>
2022-08-05 19:34:52 +0300

    user rdv et profil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,40 @@
-import {NgModule} from '@angular/core';
-import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
-import {HomeComponent} from './home/home.component';
-import {InscriptionComponent} from './inscription/inscription.component';
-import {LoginComponent} from './login/login.component';
-import {AuthGuard} from './shared/services/auth.guard';
-import {SplashComponent} from './splash/splash.component';
-import {LoggedInAuthGuard} from './shared/services/logged-in-auth.guard';
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './shared/services/auth.guard';
+import { SplashComponent } from './splash/splash.component';
+import { LoggedInAuthGuard } from './shared/services/logged-in-auth.guard';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { UserRdvComponent } from './user-rdv/user-rdv.component';
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
-  {path: 'login', component: LoginComponent, canActivate: [LoggedInAuthGuard]},
-  {path: 'inscription', component: InscriptionComponent},
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
   {
-    path: 'splash', component: SplashComponent
+    path: 'login',
+    component: LoginComponent,
+    canActivate: [LoggedInAuthGuard],
   },
+  { path: 'inscription', component: InscriptionComponent },
+  {
+    path: 'splash',
+    component: SplashComponent,
+  },
+  {
+    path: 'user-profile',
+    component: UserProfileComponent,
+  },
+  {
+    path: 'user-rdv',
+    component: UserRdvComponent,
+  }
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules}),
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
   exports: [RouterModule],
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
---
+++ src/app/user-rdv/user-rdv.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-rdv',
+  templateUrl: './user-rdv.component.html',
+  styleUrls: ['./user-rdv.component.scss'],
+})
+export class UserRdvComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
