35a6b75ee09ada6ee534b5ee5b16a4d2085212b2
zohary13 <zoharyirimboangy@gmail.com>
2022-08-07 23:43:55 +0200

    enfin fini

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiMongo: 'http://localhost:8010/rdvvaksiny',
-  apiJava: '/api/rdvvaksiny'
+  apiJava: 'https://tpt-serveur.herokuapp.com/rdvvaksiny'
 };
